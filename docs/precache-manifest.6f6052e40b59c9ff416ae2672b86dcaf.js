d89fc1b5f605e0b7a51d1cb905ccf5e156f2705d
Michał Kozłowski <michalkozlowski@Michals-MacBook-Air-2.local>
2019-07-11 16:57:19 +0200

    build

---
+++ docs/precache-manifest.6f6052e40b59c9ff416ae2672b86dcaf.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb333146af8e3376d4d8c1161d9e4aa9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "817beee175fcd608baa6",
+    "url": "./static/css/main.f0f5ba72.chunk.css"
+  },
+  {
+    "revision": "ca2c5b249aea8105e9f0",
+    "url": "./static/js/2.019cae6d.chunk.js"
+  },
+  {
+    "revision": "817beee175fcd608baa6",
+    "url": "./static/js/main.0ef1b600.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "eaf5ea766c1cb00958a8a2a09255ca38",
+    "url": "./static/media/bomb.eaf5ea76.svg"
+  },
+  {
+    "revision": "8ba79f5d3eb8daeceff502d7eec6381a",
+    "url": "./static/media/dead.8ba79f5d.svg"
+  },
+  {
+    "revision": "e9b68473221b827777f4122931040160",
+    "url": "./static/media/eight.e9b68473.svg"
+  },
+  {
+    "revision": "99712a987c9ff2a62945c0c44c89785c",
+    "url": "./static/media/five.99712a98.svg"
+  },
+  {
+    "revision": "35d811aab9bec67464a0e4555fc8097e",
+    "url": "./static/media/flag.35d811aa.svg"
+  },
+  {
+    "revision": "61cf32890d0fde4a5107c27a6d6e27be",
+    "url": "./static/media/four.61cf3289.svg"
+  },
+  {
+    "revision": "e4f19a6cc9713a32a124f56323f3e045",
+    "url": "./static/media/laughing.e4f19a6c.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "91e41c39ca732d50054e4be3dfd69ed5",
+    "url": "./static/media/one.91e41c39.svg"
+  },
+  {
+    "revision": "a1c9244b5cd2e2b9afa40f3cebba173d",
+    "url": "./static/media/seven.a1c9244b.svg"
+  },
+  {
+    "revision": "a814c6e6e286caa9c387ee8b162c4c01",
+    "url": "./static/media/six.a814c6e6.svg"
+  },
+  {
+    "revision": "e6171b46d5b5f657a8c11f44ea731e43",
+    "url": "./static/media/smile.e6171b46.svg"
+  },
+  {
+    "revision": "d3eb913d42acb6ae43d419f77d1862d6",
+    "url": "./static/media/three.d3eb913d.svg"
+  },
+  {
+    "revision": "cfcff9a13f969bfe5ce3531f621e1d5b",
+    "url": "./static/media/two.cfcff9a1.svg"
+  }
+]);
